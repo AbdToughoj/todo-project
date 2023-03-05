5188d04e871a5dbd9b09dc698dc2991961242e57
AbdToughoj <toughoj@gmail.com>
2023-03-05 18:44:49 +0000

    js2 commit

--- app.js
+++ app.js
@@ -1,30 +1,98 @@
-let userName=prompt("Please your name ");
+const userInput = [];
+let gender = null;
+let userName = null;
+
+getData();
+printData();
+
+function getData(){
+    getName();
+    getGender();
+    getAge();
+    viewWelcomeMessage();
+    getCars();
+    getSportCars();
+    getMotorbike();
+}
+
+function getName(){
+    let promptedUserName = prompt("Please enter your name ");
+    userName = promptedUserName;
+    userInput.push(promptedUserName);
+    userInput['name'] = promptedUserName
+}
+
 function getGender(){
     let genderNew = prompt('please Enter your gender ');
-    if(genderNew === 'male'){
-        return genderNew ;
-    }else if(genderNew === 'female'){
-        return genderNew ;
-    } else{
+    if(genderNew === 'male' || genderNew === 'female'){
+        gender = genderNew;
+        userInput.push(genderNew);
+    }else{
         getGender();
     }
 }
 
-let gender = getGender();
+function getAge(){
+    let age = prompt("Please enter your age")
 
-age=prompt("Please enter your age")
-
-if(age<=0 ){
-    alert("the age you entered is wrong")
+    if(age <= 0 ){
+        alert("the age you entered is wrong")
+    }
+    userInput.push(age);
 }
 
-welcome=confirm("do you want to skip the welcoming message?")
-if (welcome==false && gender=="male"){
-    alert("Welcome Mr " + userName ) 
-}else if(welcome==false && gender=="female")  {
-    alert("Welcome Ms " + userName ) 
+function viewWelcomeMessage(){
+    welcome = confirm("do you want to skip the welcoming message?");
 
+    if (welcome == false && gender == "male"){
+        alert("Welcome Mr " + userName ) 
+    }else if(welcome == false && gender == "female")  {
+        alert("Welcome Ms " + userName ) 
+    }
 }
+
+function getCars(){
+    let car_prompt = prompt("do love cars (Yes/No)");
+    let cars = car_prompt ? car_prompt : "invalid";
+    userInput.push(cars);
+}
+
+function getSportCars(){
+    let sport_prompt = prompt("do love sport cars (Yes/No)");
+    let sportCars = sport_prompt ? sport_prompt : "invalid";
+    userInput.push(sportCars); 
+}
+
+function getMotorbike(){
+    let motorbikes_prompt = prompt("do love motorbikes (Yes/No)");
+    let motorbikes = motorbikes_prompt ? motorbikes_prompt : "invalid";
+    userInput.push(motorbikes);
+}
+
+function printData(){
+    for (const input of userInput) {
+        console.log(input)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
